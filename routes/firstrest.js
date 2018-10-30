398c0f9503372516d9f1e12878b2b1868625ee87
steven <steven.bucaille.etu@univ-lille.fr>
2018-10-30 11:21:26 +0100

    Fixes

--- routes/firstrest.js
+++ routes/firstrest.js
@@ -1,9 +1,21 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/', (req, res, next) =>{
+let mysql = require('./mysql/mysqlconnection');
+
+router.get('/', (req, res, next) => {
 	console.log()
 	res.sendStatus(200);
+});
+
+router.get('/class', (req, res, next) => {
+	let classname = req;
+	console.log(classname);
+	mysql.query('select * from cd_class where cls_name LIKE "%' + classname + '%"',
+		(error, results, fields) => {
+			if (error) throw error;
+			res.send(results);
+		});
 })
 
 module.exports = router;
--- routes/mysql/mysqlconnection.js
+++ routes/mysql/mysqlconnection.js
@@ -15,4 +15,4 @@ connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
 	console.log('The solution is: ', results[0].solution);
 });
 
-connection.end();
+module.exports = connection;
