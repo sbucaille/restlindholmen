01fca04583020cbf2a3f051f1d150edb9b1c54e0
steven <steven.bucaille.etu@univ-lille.fr>
2018-11-26 21:08:38 +0100

    Initial commit for custom classes that represent entities from the database. More to come.
    Took 48 seconds

---
+++ public/javascripts/lindholmenClasses/ApiWrapper.js
@@ -0,0 +1,11 @@
+/*
+ * Developed by Steven BUCAILLE on 11/26/18 6:58 PM.
+ * Last modified 11/26/18 6:58 PM.
+ * Copyright (c) 2018. All right reserved.
+ *
+ */
+
+class ApiWrapper {
+
+
+}
---
+++ public/javascripts/lindholmenClasses/Class.js
@@ -0,0 +1,19 @@
+/*
+ * Developed by Steven BUCAILLE on 11/26/18 6:58 PM.
+ * Last modified 11/26/18 3:36 PM.
+ * Copyright (c) 2018. All right reserved.
+ *
+ */
+
+class Class {
+
+	constructor(id, name, xmiClassID, visibility) {
+		this._id = id;
+		this._name = name;
+		this._XMIClassID = xmiClassID;
+		this._visibility = visibility;
+		this._areAttributesLoaded = false;
+		this._areMethodsLoaded = false;
+	}
+
+}
