684a212c1a4a09e6888b07d217d39ed00e4ea29a
steven <steven.bucaille.etu@univ-lille.fr>
2018-11-26 21:10:16 +0100

    Updated comment
    Took 1 minute

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+/*
+ * Developed by Steven BUCAILLE on 11/26/18 6:58 PM.
+ * Last modified 11/24/18 12:24 PM.
+ * Copyright (c) 2018. All right reserved.
+ *
+ */
+
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
@@ -6,7 +13,7 @@ let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
-let mySqlConnection = require('./routes/mysql/mysqlconnection');
+let restApi = require('./routes/RestAPI/restApi');
 
 let app = express();
 
@@ -16,30 +23,33 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/rest', require('./routes/firstrest'));
+app.use('/api', restApi);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
-app.listen(8080);
+let port = process.env.PORT || 8080;
+
+app.listen(port);
+console.log("Server launched, listening on " + port);
 
 module.exports = app;
--- routes/mysql/mysqlconnection.js
+++ routes/mysql/mysqlconnection.js
@@ -1,7 +1,25 @@
+/*
+ * Developed by Steven BUCAILLE on 11/26/18 6:58 PM.
+ * Last modified 11/24/18 1:43 PM.
+ * Copyright (c) 2018. All right reserved.
+ *
+ */
+
 let mysql = require('mysql');
 let auth = require('../../auth').mysql;
+let dbshema = require('../../dbschema').db;
 
-var connection = mysql.createConnection({
+let db = require('knex')({
+	client: 'mysql',
+	connection: {
+		host 		: 'localhost',
+		user 		: auth.user,
+		password 	: auth.password,
+		database 	: auth.dbname
+	}
+});
+
+let connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : auth.user,
 	password : auth.password,
@@ -15,4 +33,8 @@ connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
 	console.log('The solution is: ', results[0].solution);
 });
 
-module.exports = connection;
+module.exports = {
+	"connection" : connection,
+	"knex" : db,
+	"dbshema" : dbshema
+};
