416f90ec5244e52b0cf8efbe938d88b2b9d41ee9
steven <steven.bucaille.etu@univ-lille.fr>
2018-11-27 16:02:16 +0100

    - Created all paths that leads to Class entity from other entities ("/api/class/fromXXXXX" url).
    Took 2 hours 40 minutes

--- routes/RestAPI/class/functions.js
+++ routes/RestAPI/class/functions.js
@@ -9,26 +9,211 @@ let knex = require('../../mysql/mysqlconnection').knex;
 let dbshema = require('../../../dbschema').db;
 let resultManipulation = require('../../mysql/resultManipulation');
 
-let getListOfClassIDs = async () => {
-	return resultManipulation.getArrayFromResult(await knex.select('id').from('cd_class'));
-};
-
-let getListOfAttributeIDs = async (classId) => {
-	return getArrayFromResult(
-		await knex.select('id').from('cd_attribute').where('cls_id', '=', classId)
-	)
-};
-
-let getListOfClassIDsFromClassDiagram = async (classDiagramID) => {
-	return getArrayFromResult(
-		await knex.select('id')
+let getAllClassesID = async () => {
+	return resultManipulation.getArrayFromResult(
+		await knex
+			.select(dbshema.contentNames.class.id)
 			.from(dbshema.tableNames.class)
-			.where(dbshema.contentNames.class.classDiagramID, '=', classDiagramID)
 	)
-}
+};
+
+let getClassesFromDiagram = async (diagramID) => {
+	return resultManipulation.getArrayFromResult(
+		await knex
+			.select(dbshema.contentNames.class.id)
+			.from(dbshema.tableNames.class)
+			.where(
+				dbshema.contentNames.class.classDiagramID,
+				'=',
+				diagramID
+			)
+	)
+};
+
+let getClassIDFromAttribute = async (attributeID) => {
+	return await knex
+		.select(dbshema.contentNames.attribute.classID)
+		.from(dbshema.tableNames.attribute)
+		.where(
+			dbshema.contentNames.attribute.id,
+			'=',
+			attributeID
+		)
+};
+
+let getClassIDFromMethod = async (methodID) => {
+	return await knex
+		.select(dbshema.contentNames.method.classID)
+		.from(dbshema.tableNames.method)
+		.where(
+			dbshema.contentNames.method.id,
+			'=',
+			methodID
+		)
+};
+
+let getClassIDFromMethodParam = async (methodParamID) => {
+	return await knex
+		.select(dbshema.contentNames.method.classID)
+		.from(dbshema.tableNames.method)
+		.innerJoin(
+			dbshema.tableNames.methodParam,
+			dbshema.tableNames.method + '.' + dbshema.contentNames.method.id,
+			'=',
+			dbshema.tableNames.methodParam + '.' + dbshema.contentNames.methodParam.methodID
+		)
+		.where(
+			dbshema.tableNames.methodParam + '.' + dbshema.contentNames.methodParam.id,
+			'=',
+			methodParamID
+		)
+};
+
+let getClassesIDFromAssociation = async (associationID) => {
+	return resultManipulation.getArrayFromResult(
+		await knex
+			.select(dbshema.contentNames.associationEnd.classID)
+			.from(dbshema.tableNames.associationEnd)
+			.innerJoin(
+				dbshema.tableNames.association,
+				dbshema.tableNames.associationEnd + '.' + dbshema.contentNames.associationEnd.associationID,
+				'=',
+				dbshema.tableNames.association + '.' + dbshema.contentNames.association.id
+			)
+			.where(
+				dbshema.tableNames.association + '.' + dbshema.contentNames.association.id,
+				'=',
+				associationID
+			)
+			.debug()
+	)
+};
+
+let getClassIDFromAssociationEnd = async (associationEndID) => {
+	return await knex
+		.select(dbshema.contentNames.associationEnd.classID)
+		.from(dbshema.tableNames.associationEnd)
+		.where(
+			dbshema.contentNames.associationEnd.id,
+			'=',
+			associationEndID
+		)
+};
+
+let getClassesIDFromDependency = async (dependencyID) => {
+	return await knex
+		.select(dbshema.contentNames.dependency.supplierClassID, dbshema.contentNames.dependency.clientClassID)
+		.from(dbshema.tableNames.dependency)
+		.where(
+			dbshema.contentNames.dependency.id,
+			'=',
+			dependencyID
+		)
+};
+
+let getSupplierClassIDFromDependency = async (dependencyID) => {
+	return await knex
+		.select(dbshema.contentNames.dependency.supplierClassID)
+		.from(dbshema.tableNames.dependency)
+		.where(
+			dbshema.contentNames.dependency.id,
+			'=',
+			dependencyID
+		)
+};
+
+let getClientClassIDFromDependency = async (dependencyID) => {
+	return await knex
+		.select(dbshema.contentNames.dependency.clientClassID)
+		.from(dbshema.tableNames.dependency)
+		.where(
+			dbshema.contentNames.dependency.id,
+			'=',
+			dependencyID
+		)
+};
+
+let getClassesIDFromRealization = async (realizationID) => {
+	return await knex
+		.select(dbshema.contentNames.realization.supplierClassID, dbshema.contentNames.realization.clientClassID)
+		.from(dbshema.tableNames.realization)
+		.where(
+			dbshema.contentNames.realization.id,
+			'=',
+			realizationID
+		)
+};
+
+let getSupplierClassIDFromRealization = async (realization) => {
+	return await knex
+		.select(dbshema.contentNames.realization.supplierClassID)
+		.from(dbshema.tableNames.realization)
+		.where(
+			dbshema.contentNames.realization.id,
+			'=',
+			realization
+		)
+};
+
+let getClientClassIDFromRealization = async (realizationID) => {
+	return await knex
+		.select(dbshema.contentNames.realization.clientClassID)
+		.from(dbshema.tableNames.realization)
+		.where(
+			dbshema.contentNames.realization.id,
+			'=',
+			realizationID
+		)
+};
+
+let getClassesIDFromGeneralization = async (generalizationID) => {
+	return await knex
+		.select(dbshema.contentNames.generalization.childClassID, dbshema.contentNames.generalization.parentClassID)
+		.from(dbshema.tableNames.generalization)
+		.where(
+			dbshema.contentNames.generalization.id,
+			'=',
+			generalizationID
+		)
+};
+
+let getChildClassIDFromGeneralization = async (generalizationID) => {
+	return await knex
+		.select(dbshema.contentNames.generalization.childClassID)
+		.from(dbshema.tableNames.generalization)
+		.where(
+			dbshema.contentNames.generalization.id,
+			'=',
+			generalizationID
+		)
+};
+
+let getParentClassIDFromGeneralization = async (generalizationID) => {
+	return await knex
+		.select(dbshema.contentNames.generalization.parentClassID)
+		.from(dbshema.tableNames.generalization)
+		.where(
+			dbshema.contentNames.generalization.id,
+			'=',
+			generalizationID
+		)
+};
 
 module.exports = {
-	getListOfClassIDs : getListOfClassIDs,
-	getListOfAttributeIDs : getListOfAttributeIDs,
-	getListOfClassIDsFromClassDiagram : getListOfClassIDsFromClassDiagram
+	getAllClassesID: getAllClassesID,
+	getClassesFromDiagram: getClassesFromDiagram,
+	getClassIDFromAttribute: getClassIDFromAttribute,
+	getClassIDFromMethod: getClassIDFromMethod,
+	getClassIDFromMethodParam: getClassIDFromMethodParam,
+	getClassesIDFromAssociation: getClassesIDFromAssociation,
+	getClassIDFromAssociationEnd: getClassIDFromAssociationEnd,
+	getClassesIDFromDependency: getClassesIDFromDependency,
+	getSupplierClassIDFromDependency: getSupplierClassIDFromDependency,
+	getClientClassIDFromDependency: getClientClassIDFromDependency,
+	getClassesIDFromRealization: getClassesIDFromRealization,
+	getSupplierClassIDFromRealization: getSupplierClassIDFromRealization,
+	getClientClassIDFromRealization: getClientClassIDFromRealization,
+	getClassesIDFromGeneralization: getClassesIDFromGeneralization,
+	getChildClassIDFromGeneralization: getChildClassIDFromGeneralization,
+	getParentClassIDFromGeneralization: getParentClassIDFromGeneralization
 };
--- routes/RestAPI/class/rest.js
+++ routes/RestAPI/class/rest.js
@@ -10,24 +10,85 @@
 let express = require('express');
 let router = express.Router();
 
-let mysql = require('../../mysql/mysqlconnection');
 let classRequests = require('./functions');
 
-router.get('/all', async (req, res, next) => {
-	let listOfId = await classRequests.getListOfClassIDs();
-	res.send(listOfId);
+router.get('/allID', async (req, res, next) => {
+	res.send(await classRequests.getAllClassesID())
 });
 
-router.get('/fromClassDiagram', async(req, res, next) => {
-	let classDiagramID = req.query.classDiagramID;
-	let results = await classRequests.getListOfClassIDsFromClassDiagram(classDiagramID);
-	res.send(results);
-})
+router.get('/fromDiagram', async (req, res, next) => {
+	let diagramID = req.query.diagramID;
+	res.send(await classRequests.getClassesFromDiagram(diagramID))
+});
 
-router.get('/attributes', async (req, res, next) => {
-	let classId = req.query.classId;
-	let attributes = await classRequests.getListOfAttributeIDs(classId);
-	res.send(attributes);
+router.get('/fromAttribute', async (req, res, next) => {
+	let attributeID = req.query.attributeID;
+	res.send(await classRequests.getClassIDFromAttribute(attributeID))
+});
+
+router.get('/fromMethod', async (req, res, next) => {
+	let methodID = req.query.methodID;
+	res.send(await classRequests.getClassIDFromMethod(methodID))
+});
+
+router.get('/fromMethodParam', async (req, res, next) => {
+	let methodParamID = req.query.methodParamID;
+	res.send(await classRequests.getClassIDFromMethodParam(methodParamID))
+});
+
+router.get('/fromAssociation', async (req, res, next) => {
+	let associationID = req.query.associationID;
+	res.send(await classRequests.getClassesIDFromAssociation(associationID))
+});
+
+router.get('/fromAssociationEnd', async (req, res, next) => {
+	let associationEndID = req.query.associationEndID;
+	res.send(await classRequests.getClassIDFromAssociationEnd(associationEndID))
+});
+
+router.get('/fromDependency', async (req, res, next) => {
+	let dependencyID = req.query.dependencyID;
+	res.send(await classRequests.getClassesIDFromDependency(dependencyID))
+});
+
+router.get('/fromDependency/supplierClass', async (req, res, next) => {
+	let dependencyID = req.query.dependencyID;
+	res.send(await classRequests.getSupplierClassIDFromDependency(dependencyID))
+});
+
+router.get('/fromDependency/clientClass', async (req, res, next) => {
+	let dependencyID = req.query.dependencyID;
+	res.send(await classRequests.getClientClassIDFromDependency(dependencyID))
+});
+
+router.get('/fromRealization', async (req, res, next) => {
+	let dependencyID = req.query.realizationID;
+	res.send(await classRequests.getClassesIDFromRealization(dependencyID))
+});
+
+router.get('/fromRealization/supplierClass', async (req, res, next) => {
+	let dependencyID = req.query.realizationID;
+	res.send(await classRequests.getSupplierClassIDFromRealization(dependencyID))
+});
+
+router.get('/fromRealization/clientClass', async (req, res, next) => {
+	let dependencyID = req.query.realizationID;
+	res.send(await classRequests.getClientClassIDFromRealization(dependencyID))
+});
+
+router.get('/fromGeneralization', async (req, res, next) => {
+	let generalizationID = req.query.generalizationID;
+	res.send(await classRequests.getClassesIDFromGeneralization(generalizationID))
+});
+
+router.get('/fromGeneralization/childClass', async (req, res, next) => {
+	let dependencyID = req.query.generalizationID;
+	res.send(await classRequests.getChildClassIDFromGeneralization(generalizationID))
+});
+
+router.get('/fromGeneralization/parentClass', async (req, res, next) => {
+	let dependencyID = req.query.generalizationID;
+	res.send(await classRequests.getParentClassIDFromGeneralization(generalizationID))
 });
 
 module.exports = router;
