6bf828436a273cc32bd70fcb0db99f31625041d4
steven <steven.bucaille.etu@univ-lille.fr>
2019-01-30 16:46:58 +0100

    Bug fix.
    Took 54 seconds

--- routes/mysql/requestHandler.js
+++ routes/mysql/requestHandler.js
@@ -9,22 +9,31 @@ let resultManipulation = require('./resultManipulation');
 
 let handleParameter = async (parameter, requestFunction) => {
 
-	if (!Array.isArray(parameter)) parameter = [parameter];
-	let requestResult = await requestFunction(parameter);
-	return resultManipulation.getResultWithLinkedParameter(requestResult);
+    if (!Array.isArray(parameter)) parameter = [parameter];
+    let requestResult = await requestFunction(parameter);
+    return resultManipulation.getResultWithLinkedParameter(requestResult);
 };
 
-let getDataFromQuery = (request, data) => {
-	if (request.body) return requestDataToArray(request.body[data]);
-	else if (request.query) return requestDataToArray(JSON.parse(request.query)[data]);
-	else throw data + " parameter could not be found.";
+let getDataFromQuery = (request, parameter) => {
+    if (request.body) {
+        if(request.body[parameter]) return requestDataToArray(request.body, parameter);
+        else throw parameter + " parameter could not be found.";
+    }
+    else if (request.query) {
+        if(request.query[parameter]) return requestDataToArray(JSON.parse(request.query, parameter));
+        else throw parameter + " parameter could not be found.";
+    }
+    else throw "No parameter found in body or query";
 };
 
-let requestDataToArray = (data) => {
-	if(!Array.isArray(data)) return [data];
-	else return data;
+let requestDataToArray = (data, parameter) => {
+    if (!data.filter){
+        if (!Array.isArray(data[parameter])) return [data[parameter]];
+        else return data[parameter];
+    }
+    else return data;
 }
 module.exports = {
-	handleParameter: handleParameter,
-	getDataFromQuery: getDataFromQuery
+    handleParameter: handleParameter,
+    getDataFromQuery: getDataFromQuery
 };
